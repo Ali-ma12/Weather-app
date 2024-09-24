7e6861126c5e5c7debc9bee6f51e749817e0ea28
Ali-ma12 <ali.radi.1423@outlook.com>
2024-09-24 23:53:10 +0300

    Added forcast card.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/forcast.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: const Text('Weather App'),
+        ),
+        body: const Column(children: [ForcastCard()],),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/forcast.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:weather_icons/weather_icons.dart';
+
+class ForcastCard extends StatelessWidget {
+  const ForcastCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      padding: const EdgeInsets.all(16),
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.25,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.purpleAccent,
+            Color.fromARGB(255, 112, 44, 213),
+          ],
+        ),
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BoxedIcon(
+                WeatherIcons.day_rain,
+                size: 100,
+                color: Colors.white,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Cloudy',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  Text(
+                    'Sep, 24, 2024',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '24°',
+                style: TextStyle(
+                    fontSize: 90,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    height: 1),
+              ),
+              Text(
+                'Feels like 26 °',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Row(
+                children: [
+                  Opacity(
+                    opacity: 0.4,
+                    child: BoxedIcon(
+                      WeatherIcons.snowflake_cold,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                  ),
+                  Opacity(
+                    opacity: 0.4,
+                    child: BoxedIcon(
+                      WeatherIcons.cloudy_windy,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
